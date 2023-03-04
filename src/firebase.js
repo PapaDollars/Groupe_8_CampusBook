4009e92b14c4e4698f1eb4bde2f6f337e9f59fc4
PapaDollars <iyadaniel51@gmail.com>
2023-03-04 09:38:40 +0100

    first depot
    Publication des contenus (textes, photos, vidéos);
    S'abonner aux utilisateurs de la plateforme et consulter leurs contenus également.;
    Liker et commenter les publications;
    Voir le nombre de following;
    Créer son compte.

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX_My0mAyFfRjHCAGDdYRqztoMQTbeoPw",
+  authDomain: "social-media-a024e.firebaseapp.com",
+  projectId: "social-media-a024e",
+  storageBucket: "social-media-a024e.appspot.com",
+  messagingSenderId: "410474749180",
+  appId: "1:410474749180:web:3ae7b0feb937effa0d4b79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
